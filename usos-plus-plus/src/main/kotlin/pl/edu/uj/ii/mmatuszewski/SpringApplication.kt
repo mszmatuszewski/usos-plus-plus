5a3be4d54baf0ebbefb6a1f50dfd16d81dfcdae2
Michał Matuszewski <mszmatuszewski@gmail.com>
2019-01-03 20:05:48 +0100

    Revert "Enabled debug for Spring Security"
    This reverts commit 9e27d0db

--- usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/SpringApplication.kt
+++ usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/SpringApplication.kt
@@ -5,13 +5,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableCaching
-@EnableWebSecurity(debug = true)
 class SpringApplication
 
 fun main(args: Array<String>) {
