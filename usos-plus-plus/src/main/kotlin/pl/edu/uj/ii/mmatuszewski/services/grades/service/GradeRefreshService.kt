7c900f6bba5ae01dded90ab35017648864c49045
Michał Matuszewski <mszmatuszewski@gmail.com>
2019-01-03 18:51:33 +0100

    Added grade caching

--- usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/services/grades/service/GradeRefreshService.kt
+++ usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/services/grades/service/GradeRefreshService.kt
@@ -1,5 +1,6 @@
 package pl.edu.uj.ii.mmatuszewski.services.grades.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -10,12 +11,15 @@ class GradeRefreshService(
         private val courseProviderService: CourseProviderService,
         private val userRepository: UserRepository) {
 
+    private val LOGGER = LoggerFactory.getLogger(GradeRefreshService::class.java)
+
     @CacheEvict(allEntries = true, value = ["grades"], beforeInvocation = true)
     @Scheduled(fixedRate = 60 * 60 * 1000)
     fun refresh() {
         userRepository.findAll()
                 .filter { it.usosAccessToken != null }
                 .map { it.username }
+                .onEach { LOGGER.info("Refreshing grades for user: $it") }
                 .forEach { courseProviderService.provide(it) }
     }
 }
