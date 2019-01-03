d8b1e8e3a6913864460904c7d743aac5225fd57c
Michał Matuszewski <mszmatuszewski@gmail.com>
2019-01-03 16:45:54 +0100

    Added logging

--- usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/core/usos/model/UsosAuthenticationDetailsProvider.kt
+++ usos-plus-plus/src/main/kotlin/pl/edu/uj/ii/mmatuszewski/core/usos/model/UsosAuthenticationDetailsProvider.kt
@@ -1,11 +1,15 @@
 package pl.edu.uj.ii.mmatuszewski.core.usos.model
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 @ConfigurationProperties(prefix = "usos.consumer")
 class UsosAuthenticationDetailsProvider {
+
+    private val LOGGER = LoggerFactory.getLogger(UsosAuthenticationDetailsProvider::class.java)
 
     lateinit var key: String
     lateinit var secret: String
@@ -16,4 +20,16 @@ class UsosAuthenticationDetailsProvider {
 
     lateinit var appUrl: String
     lateinit var scopes: String
+
+    @PostConstruct
+    fun logDetails() {
+        LOGGER.info("Initialized UsosAuthenticationDetailsProvider")
+        LOGGER.info("usos.consumer.key=${key}")
+        LOGGER.info("usos.consumer.secret=${secret}")
+        LOGGER.info("usos.consumer.requestTokenUrl=${requestTokenUrl}")
+        LOGGER.info("usos.consumer.userAuthUrl=${userAuthUrl}")
+        LOGGER.info("usos.consumer.accessTokenUrl=${accessTokenUrl}")
+        LOGGER.info("usos.consumer.appUrl=${appUrl}")
+        LOGGER.info("usos.consumer.scopes=${scopes}")
+    }
 }
